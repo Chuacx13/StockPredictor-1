937925f3e26ab5d8b1833eb408735d5e92682360
Chuacx13 <chuachongxuan@gmail.com>
2024-03-09 07:10:49 +0800

    Completed RMSE and Adjusted R^2 evaluation of train and test set

--- prediction.py
+++ prediction.py
@@ -45,11 +45,11 @@ def data_munging_stocks(target_stock_name, target_stock_data):
     df[target_stock_name] = target_stock_data['Open'].shift(-1) - target_stock_data['Open']
     df[target_stock_name + "_lag"] = df[target_stock_name].shift(1)
 
-    window = 10  
+    window = 20
     df[f'{target_stock_name}_SMA_{window}'] = target_stock_data['Close'].rolling(window=window).mean()
     df[f'{target_stock_name}_EMA_{window}'] = target_stock_data['Close'].ewm(span=window, adjust=False).mean()
 
-    window = 50  
+    window = 200
     df[f'{target_stock_name}_SMA_{window}'] = target_stock_data['Close'].rolling(window=window).mean()
     df[f'{target_stock_name}_EMA_{window}'] = target_stock_data['Close'].ewm(span=window, adjust=False).mean()
 
@@ -79,18 +79,52 @@ def data_munging_stocks(target_stock_name, target_stock_data):
 
     return df_scaled, scaler_y, df
 
-def predict_gain(df, scaler): 
-    train_index = round(len(df) * 0.7)
-    test_index = train_index + 30
-    X_train = df.iloc[:train_index, 1:]
-    X_test = df.iloc[test_index:, 1:]
-    y_train = df.iloc[:train_index, 0]
-    y_test = df.iloc[test_index:, 0]
-    rf =  RandomForestRegressor(n_estimators=1000)
-    rf.fit(X_train, y_train)
-    predictions = rf.predict(X_test)
-    unscaled_predictions = scaler.inverse_transform(predictions.reshape(-1, 1)).flatten()
-    return unscaled_predictions
+# Prepare train scaled set, test scaled set, train data, test data and model
+def prep_train_test_model(df_scaled, df): 
+    proportion_test = 0.8
+    gap = 180
+    train_index = round(len(df_scaled) * proportion_test)
+    test_index = train_index + gap
+    X_scaled_train = df_scaled.iloc[:train_index, 1:]
+    X_scaled_test = df_scaled.iloc[test_index:, 1:]
+    y_scaled_train = df_scaled.iloc[:train_index, 0]
+    y_scaled_test = df_scaled.iloc[test_index:, 0]
+
+    train = df.iloc[:train_index, :]
+    test = df.iloc[test_index:, :]  
+
+    rf = RandomForestRegressor(n_estimators=1000)
+    rf.fit(X_scaled_train, y_scaled_train)
+    return X_scaled_train, X_scaled_test, y_scaled_train, y_scaled_test, train, test, rf # might not need y
     
+def adjusted_metric(scaled_data, data, model, num_of_predictors, yname, scaler_y):  
+    predictions = model.predict(scaled_data)
+    unscaled_predictions = scaler_y.inverse_transform(predictions.reshape(-1, 1)).flatten()
+    data.loc[:,'predicted_gain'] = unscaled_predictions
+
+    SST = ((data[yname] - data[yname].mean())**2).sum()
+    SSR = ((data['predicted_gain'] - data[yname].mean())**2).sum()
+    SSE = ((data[yname] - data['predicted_gain'])**2).sum()
+    r2 = SSR/SST
+
+    adjustR2 = 1 - (1 - r2)*(data.shape[0] - 1)/(data.shape[0] - num_of_predictors - 1)
+    RMSE = (SSE/(data.shape[0] - num_of_predictors - 1))**0.5
+    return adjustR2, RMSE
+
+def assess_table(train_scaled_data, test_scaled_data, train_data, test_data, model, yname, scaler_y):
+    num_of_predictors = len(train_data.columns) - 1
+    r2test, RMSEtest = adjusted_metric(test_scaled_data, test_data, model, num_of_predictors, yname, scaler_y)
+    r2train, RMSEtrain = adjusted_metric(train_scaled_data, train_data, model, num_of_predictors, yname, scaler_y)
+
+    assessment = pd.DataFrame(index=['R2', 'RMSE'], columns=['Train', 'Test'])
+    assessment['Train'] = [r2train, RMSEtrain]
+    assessment['Test'] = [r2test, RMSEtest]
+    return assessment
+
+#Test Output
 result = data_munging_stocks("AAPL", yf.download("AAPL", START, TODAY))
-print(result[0], result[2])
+gain = prep_train_test_model(result[0], result[2])
+final = assess_table(gain[0], gain[1], gain[4], gain[5], gain[6], "AAPL", result[1])
+print(final)
+
+
